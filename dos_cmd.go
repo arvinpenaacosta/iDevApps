b95f34ccf407ebb6e7918d66d1f953f5a7a6db17
arvinpenaacosta <117697006+arvinpenaacosta@users.noreply.github.com>
2022-11-08 17:53:22 +0800

    Update dos_cmd.go

--- dos_cmd.go
+++ dos_cmd.go
@@ -8,12 +8,18 @@ import (
 
 func main() {
 
-  // Show Product Key
-  dos_cmd := "wmic path SoftwareLicensingService get OA3xOriginalProductKey"
-  // Show Serial Number
-	//dos_cmd := "wmic bios get serialnumber"
+  	// Show Product Key
+  	dos_cmd := "wmic path SoftwareLicensingService get OA3xOriginalProductKey"
+
+	// Show Serial Number
+  	//dos_cmd := "wmic bios get serialnumber"
     
-  cmdr := exec.Command("cmd", "/c", dos_cmd)  //copy read file to myfile.txt 
-  cmdr.Stdout = os.Stdout
-  cmdr.Run()
+  	cmdr := exec.Command("cmd", "/c", dos_cmd)  //copy read file to myfile.txt 
+	cmdr.Stdout = os.Stdout
+  	cmdr.Run()
 }
+
+/*
+choose which dos command you wish to run @ Line 12 & Line 15
+
+*/
